61817bca68b299215d5002ca5d35e367f2b381ea
Roger Levy <roger.levy@gmail.com>
2018-05-21 17:19:45 -0400

    Add models

---
+++ models/forum.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var ForumSchema = new Schema({
+        title: String,
+        author: Object,
+        comments: Object,
+        subscribers: Array,
+        date: Date
+    });
+
+ForumSchema.statics.getAll = function(callback) {
+    this.find({}, callback).sort([['date', -1]]);
+};
+
+ForumSchema.statics.getOne = function(id, callback) {
+    this.findOne({_id: id}, callback);
+};
+
+ForumSchema.statics.addForumReply = function(id, user, comment, callback) {
+    this.update({_id: id}, {$push: {comments: {user: user, comment: comment, date: new Date()}}}, callback);
+};
+
+ForumSchema.statics.addSubscriber = function(id, user, callback) {
+    this.update({_id: id}, {$addToSet:{subscribers: user}}, callback);
+};
+
+ForumSchema.statics.removeSubscriber = function(id, user, callback) {
+    this.update({_id: id}, {$pull:{subscribers: user}}, callback);
+};
+
+var Forum = mongoose.model('forum', ForumSchema, 'forum');
+module.exports = Forum;
---
+++ models/user.js
@@ -0,0 +1,57 @@
+var mcapi = require( 'mailchimp-api/mailchimp' );
+var mc = new mcapi.Mailchimp( process.env.MAILCHIMP_PRIVATE_KEY );
+
+var mongoose = require('mongoose'),
+    sha1 = require('sha1'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    username: {
+        type: String,
+        trim: true,
+        set: function( username ) {                 // save the previous value of the email address
+            this.oldUsername = this.username;       // so that we can use it to identify the user with MailChimp
+            return username;                        // when updating their email address.
+        }
+    },
+    password: String,
+    web_address: String,
+    organization: Object,
+    hide_organization: Boolean,
+    active: Boolean,
+    admin: Boolean,
+    stripe: {
+        customer: Object,
+        subscription: Object
+    },
+    expires: Date,
+    expired: Boolean,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
+});
+
+UserSchema.virtual('fullName').get(function() {
+    return this.first_name + ' ' + this.last_name;
+});
+
+UserSchema.virtual('email_address').get(function() {
+    return this.username
+});
+
+UserSchema.methods.authenticate = function(password) {
+    return this.password === sha1(password);
+};
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
+
+UserSchema.pre('save', function(next) {
+    if(this.isModified('password')) {
+        this.password = sha1(this.password);
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema, 'User');
